f061dca93bfc9860cf8faa96bf323d2fa5f6c2a0
Tim Etchells <tetchell@redhat.com>
2021-01-20 20:29:58 -0500

    Tag CI images with short_sha and use latest for dev
    Signed-off-by: Tim Etchells <tetchell@redhat.com>

--- runners/build-images.sh
+++ runners/build-images.sh
@@ -3,7 +3,7 @@
 set -eE -o pipefail
 
 REGISTRY=${RUNNERS_REGISTRY:-quay.io/redhat-github-actions}
-TAG=${RUNNERS_TAG:-dev}     # 'latest' refers to latest CI build on main branch.
+TAG=${RUNNERS_TAG:-latest}
 
 BASE_IMG=${REGISTRY}/redhat-actions-runner:${TAG}
 BUILDAH_IMG=${REGISTRY}/redhat-buildah-runner:${TAG}
@@ -13,14 +13,14 @@ set -x
 cd $(dirname $0)
 
 docker build ./base -f ./base/Dockerfile -t $BASE_IMG
-docker build ./buildah -f ./buildah/Dockerfile -t $BUILDAH_IMG
+# docker build ./buildah -f ./buildah/Dockerfile -t $BUILDAH_IMG
 
 set +x
 
 if [[ $1 == "push" ]]; then
     set -x
     docker push $BASE_IMG
-    docker push $BUILDAH_IMG
+    # docker push $BUILDAH_IMG
 else
     echo "Not pushing. Set \$1 to 'push' to push"
 fi
